5a8c974bcabd863001fb5c709c6f780f95abf969
Milosz08 <gilgamilosz451@gmail.com>
2022-09-02 23:16:09 +0200

    move created variables into separate configuration class

---
+++ src/main/java/pl/miloszgilga/chessappbackend/config/EnvironmentVars.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: Environment.java
+ * Last modified: 02/09/2022, 22:41
+ * Project name: chess-app-backend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.chessappbackend.config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+@Getter
+@Component
+@NoArgsConstructor
+public class EnvironmentVars {
+    @Value("${config.authorization.jwt-key}")                                   private String jwtSecretKey;
+    @Value("${authorization.one-time-access-token.token-length}")               private int otaTokenLenght;
+    @Value("${authorization.one-time-access-token.token-expired-minutes}")      private int otaTokenExpiredMinutes;
+    @Value("${spring.profiles.active}")                                         public String applicationMode;
+}
