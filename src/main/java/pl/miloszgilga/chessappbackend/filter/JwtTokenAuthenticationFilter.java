76bba860b5607c436e61a9c4dbf4adcc5a0156a3
Milosz08 <gilgamilosz451@gmail.com>
2022-09-28 21:02:46 +0200

    handling exceptions in authentication filter (with custom annotation, which allows to pass different user type in controller method)

--- src/main/java/pl/miloszgilga/chessappbackend/filter/JwtTokenAuthenticationFilter.java
+++ src/main/java/pl/miloszgilga/chessappbackend/filter/JwtTokenAuthenticationFilter.java
@@ -18,13 +18,15 @@
 
 package pl.miloszgilga.chessappbackend.filter;
 
+import com.nimbusds.common.contenttype.ContentType;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.util.StringUtils;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -35,10 +37,15 @@ import java.util.Arrays;
 import java.io.IOException;
 
 import pl.miloszgilga.chessappbackend.oauth.AuthUser;
+import pl.miloszgilga.chessappbackend.utils.TimeHelper;
+import pl.miloszgilga.chessappbackend.utils.StringManipulator;
+import pl.miloszgilga.chessappbackend.exception.ServerExceptionRes;
 import pl.miloszgilga.chessappbackend.token.JsonWebTokenVerificator;
 import pl.miloszgilga.chessappbackend.security.AuthUserDetailService;
+import pl.miloszgilga.chessappbackend.exception.BasicDataExceptionRes;
 import pl.miloszgilga.chessappbackend.token.dto.UserVerficationClaims;
 
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static pl.miloszgilga.chessappbackend.security.SecurityConfiguration.DISABLE_PATHS_FOR_JWT_FILTERING;
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -49,10 +56,15 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
     private static final String TOKEN_HEADER = "Authorization";
     private static final String TOKEN_PREFIX = "Bearer ";
 
+    private final TimeHelper timeHelper;
+    private final StringManipulator manipulator;
     private final JsonWebTokenVerificator verificator;
     private final AuthUserDetailService userDetailService;
 
-    public JwtTokenAuthenticationFilter(JsonWebTokenVerificator verificator, AuthUserDetailService userDetailService) {
+    public JwtTokenAuthenticationFilter(TimeHelper timeHelper, AuthUserDetailService userDetailService,
+                                        JsonWebTokenVerificator verificator, StringManipulator manipulator) {
+        this.timeHelper = timeHelper;
+        this.manipulator = manipulator;
         this.verificator = verificator;
         this.userDetailService = userDetailService;
     }
@@ -62,13 +74,28 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
             throws IOException, ServletException {
 
         final String token = extractJwtTokenFromRequest(req);
-        if (StringUtils.hasText(token) && !verificator.basicTokenIsMalformed(token)) {
-            final UserVerficationClaims verficationClaims = verificator.validateUserJwtFilter(token);
-            final AuthUser authUser = userDetailService.loadUserByNicknameEmailAndId(verficationClaims);
-            final var authToken = new UsernamePasswordAuthenticationToken(authUser, null, authUser.getAuthorities());
-            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+        try {
+            if (StringUtils.hasText(token) && !verificator.basicTokenIsMalformed(token)) {
+                final UserVerficationClaims verficationClaims = verificator.validateUserJwtFilter(token);
+                final AuthUser authUser = userDetailService.loadUserByNicknameEmailAndId(verficationClaims);
+                final var authToken = new UsernamePasswordAuthenticationToken(authUser, authUser.getUserModel().getId());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } catch (Exception ex) {
+            final var serverExceptionRes = ServerExceptionRes.builder()
+                    .path(req.getServletPath())
+                    .method(req.getMethod())
+                    .statusCode(UNAUTHORIZED.value())
+                    .statusText(UNAUTHORIZED.name())
+                    .servletTimestampUTC(timeHelper.getCurrentUTC())
+                    .build();
+            final var basicDataExceptionRes = new BasicDataExceptionRes(serverExceptionRes, ex.getMessage());
+            SecurityContextHolder.clearContext();
 
-            SecurityContextHolder.getContext().setAuthentication(authToken);
+            res.setContentType(ContentType.APPLICATION_JSON.getType());
+            res.setStatus(UNAUTHORIZED.value());
+            res.getWriter().write(manipulator.convertObjectToJson(basicDataExceptionRes));
         }
         filterChain.doFilter(req, res);
     }
